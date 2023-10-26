75cab4c9a9520c052b714870ccfda8cee374e22b
Afobul <afonsobulhoes07@gmail.com>
2023-10-26 15:34:03 +0000

    trabalho

---
+++ jogo_euro-milhões.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+while true
+do
+    echo "Gerando números para o Euro-Milhões:"
+    echo "Números:"
+    for i in {1..5}; do
+        echo $((RANDOM % 50 + 1))
+    done
+
+    echo "Estrelas:"
+    for i in {1..2}; do
+        echo $((RANDOM % 12 + 1))
+    done
+
+    read -p "Deseja gerar mais uma sequência? (s/n): " choice
+    if [ "$choice" != "s" ]; then
+        echo "Boa Sorte!"
+        break
+    fi
+done
